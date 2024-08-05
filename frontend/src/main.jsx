7fad9e1595489b4066a16d8e68ba6e258e32565b
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-08-05 21:17:41 +1000

    frontend setup done, auth login/signup tabs added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import App from "./App";
+import Auth from "./pages/Auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        path: "/auth",
+        element: <Auth />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
 });
