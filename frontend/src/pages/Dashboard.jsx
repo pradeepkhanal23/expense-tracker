5eb24e3805843c80da3ab0505d60aa7d39a0e8b5
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-08-08 10:15:45 +1000

    once we get access to the dashboard page, we are fetching the user details and saving it to the user state for efective handling of info throught the page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,34 @@
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { useState, useEffect } from "react";
+
 const Dashboard = () => {
+  const [user, setUser] = useState({});
+  //fetching the user data
+  const { loading, data } = useQuery(GET_ME);
+
+  // fetch the user details on component render
+  useEffect(() => {
+    if (data && data.me) {
+      setUser(data.me);
+    }
+  }, [data]);
+
+  if (loading) {
+    return (
+      <>
+        <h1>Loading..</h1>
+      </>
+    );
+  }
+
   return (
     <>
-      <h1>Dashboard</h1>
+      {user && (
+        <>
+          <p>Welcome, {user.username}!!</p>
+        </>
+      )}
     </>
   );
 };
